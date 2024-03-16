8f4208d7499b02f787088f6ca4afd99fb3c8af9a
mukhammadjondev <muhsdev@gmail.com>
2024-03-16 14:04:53 +0500

    Marketing page

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button';
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pb-7 pt-8 pl-4 flex items-center gap-x-3">
+          <Image src="/mascot.svg" alt="Mascot" width={40} height={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
+}
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,18 @@
+import Footer from './footer';
+import Header from './header';
+
 type Props = {
   children: React.ReactNode;
 };
 
 export default function MarketingLayout({ children }: Props) {
   return (
-    <main>
-      <div>{children}</div>
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 }
