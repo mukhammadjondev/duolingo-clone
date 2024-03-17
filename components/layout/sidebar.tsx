cee5b004b6187d881635cf88eb3720a466d4278d
mukhammadjondev <muhsdev@gmail.com>
2024-03-17 11:09:17 +0500

    Sidebar

---
+++ components/layout/sidebar.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { Loader } from 'lucide-react';
+import { ClerkLoaded, ClerkLoading, UserButton } from '@clerk/nextjs';
+import { SidebarItem } from './sidebar-item';
+
+type Props = {
+  className?: string;
+};
+
+export const Sidebar = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2',
+        className
+      )}
+    >
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem label="quests" href="/quests" iconSrc="/quests.svg" />
+        <SidebarItem label="shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
+    </div>
+  );
+};
